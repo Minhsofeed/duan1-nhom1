209b9c398b5536ae5852b40f492559d448660da1
Phcus.05 <ducphcus@gmail.com>
2024-12-06 17:04:17 +0700

    chỉnh sửa lại dao diện trang chủ

--- mvc-oop-basic/views/client/layout/footer.php
+++ mvc-oop-basic/views/client/layout/footer.php
@@ -9,25 +9,23 @@
                             <a href="#"><img src="client/client/client/assets/img/logo/logo.png" alt=""></a>
                         </div>
                         <div class="footer_contact">
-                            <p>We are a team of designers and developers that
-                                create high quality Magento, Prestashop, Opencart...</p>
-                            <p><span>Address</span> 4710-4890 Breckinridge St, UK Burlington, VT 05401</p>
-                            <p><span>Need Help?</span>Call: <a href="tel:1-800-345-6789">1-800-345-6789</a></p>
+                            <p>Chúng tôi là một nhóm thiết kế và lập trình</p>
+                            <p><span>Địa Chỉ</span>Nam Từ Liêm,Hà Nội</p>
+                            <p><span>Cần Trợ Giúp</span>Liên Hệ: <a href="tel:1-800-345-6789">1-800-345-6789</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-6 col-sm-6">
                     <div class="widgets_container widget_menu">
-                        <h3>Information</h3>
+                        <h3>Thông Tin</h3>
                         <div class="footer_menu">
                             <ul>
-                                <li><a href="about.html">About Us</a></li>
-                                <li><a href="#">Delivery Information</a></li>
-                                <li><a href="privacy-policy.html">privacy policy</a></li>
-                                <li><a href="coming-soon.html">Coming Soon</a></li>
-                                <li><a href="#">Terms & Conditions</a></li>
-                                <li><a href="#">Returns</a></li>
-                                <li><a href="#">Gift Certificates</a></li>
+                                <li><a href="about.html">Về Chúng Tôi</a></li>
+                                <li><a href="#">Thông Tin Giao Hàng</a></li>
+                                <li><a href="privacy-policy.html">Chính Sách Bảo Mật</a></li>
+                                <li><a href="#">Điều khoản & Điều kiện</a></li>
+                                <li><a href="#">Quay lại</a></li>
+                                <li><a href="#">Quà Tặng</a></li>
                             </ul>
                         </div>
                     </div>
@@ -37,24 +35,24 @@
                         <h3>Extras</h3>
                         <div class="footer_menu">
                             <ul>
-                                <li><a href="#">Returns</a></li>
-                                <li><a href="#">Order History</a></li>
-                                <li><a href="wishlist.html">Wish List</a></li>
-                                <li><a href="#">Newsletter</a></li>
-                                <li><a href="#">Affiliate</a></li>
-                                <li><a href="#">Specials</a></li>
+                                <li><a href="#">Quay lại</a></li>
+                                <li><a href="#">Lịch Sử Giao Dich</a></li>
+                                <li><a href="wishlist.html">Danh sách mong muốn</a></li>
+                                <li><a href="#">Bản Tin</a></li>
+                                <li><a href="#">Liên Kết</a></li>
+                                <li><a href="#">Đặc Biệt</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="widgets_container">
-                        <h3>Newsletter Subscribe</h3>
-                        <p>We’ll never share your email address with a third-party.</p>
+                        <h3>Đăng Ký Để Nhận Được thông báo</h3>
+                        <p>Chúng tôi sẽ không bao giờ chia sẻ địa chỉ email của bạn với bên thứ ba.</p>
                         <div class="subscribe_form">
                             <form id="mc-form" class="mc-form footer-newsletter">
-                                <input id="mc-email" type="email" autocomplete="off" placeholder="Enter you email address here..." />
-                                <button id="mc-submit">Subscribe</button>
+                                <input id="mc-email" type="email" autocomplete="off" placeholder="Điền Email của bạn vào đây..." />
+                                <button id="mc-submit">Đăng Ký</button>
                             </form>
                             <!-- mailchimp-alerts Start -->
                             <div class="mailchimp-alerts text-centre">
